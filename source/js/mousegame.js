d6f281898d6e63ebbe9c179c1992188b8db1d3e7
jaik <cwh88cwh88@gmail.com>
2021-12-14 12:10:56 +0800

    mousegame

---
+++ source/js/mousegame.js
@@ -0,0 +1,20 @@
+document.addEventListener('mousemove',e => {
+    const item = document.querySelectorAll(".item")
+
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    item.forEach(sqr => {
+        const sqrX = sqr.offsetLeft
+        const sqrY = sqr.offsetTop
+
+        const diffX = mouseX -sqrX
+        const diffY = mouseY -sqrY
+
+        const radians = Math.atan2(diffY,diffX)
+
+        const angle = radians * 180 / Math.PI
+
+        sqr.style.transform = `rotate(${angle}deg)`
+    })
+})
