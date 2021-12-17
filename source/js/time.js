7c03688e92b617cb92ead82f52e1a0780bc7c677
jaik <cwh88cwh88@gmail.com>
2021-12-17 19:56:06 +0800

    v6.1

--- source/js/time.js
+++ source/js/time.js
@@ -1,64 +1,34 @@
-const days = ["星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
-const months = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-//获取元素
-const btn = document.querySelector('button');
-const shizhong = document.querySelector('.shizhong');
-const body = document.querySelector('body');
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minute');
-const secondsEl = document.querySelector('.seconds');
+const year = document.querySelector('.year');
 const time = document.querySelector('.time');
-const shijianduan = document.querySelector('.shijianduan');
-const shijian = document.querySelector('.shijian');
-const dataEl = document.querySelector('.data');
-let showTime = (hour) => {
-        if (hour > 3 && hour <= 6) {
-            shijianduan.innerHTML = '凌晨';
-        } else if (hour > 6 && hour <= 8) {
-            shijianduan.innerHTML = '早晨';
-        } else if (hour > 8 && hour <= 11) {
-            shijianduan.innerHTML = '上午';
-        } else if (hour > 11 && hour <= 13) {
-            shijianduan.innerHTML = '中午';
-        } else if (hour > 13 && hour <= 17) {
-            shijianduan.innerHTML = '下午';
-        } else if (hour > 17 && hour <= 19) {
-            shijianduan.innerHTML = '傍晚';
-        } else if (hour > 19 && hour <= 23) {
-            shijianduan.innerHTML = '晚上';
-        } else if (hour > 23 && hour <= 3) {
-            shijianduan.innerHTML = '深夜';
-        }
-    }
-    //获取时间函数
-let setDate = () => {
-        const date = new Date();
-        const month = date.getMonth(); //10
-        const weeks = date.getDay(); //星期
-        const day = date.getDate() //几号
-        let hour = date.getHours();
-        hour = hour < 10 ? '0' + hour : hour;
-        let minute = date.getMinutes();
-        minute = minute < 10 ? '0' + minute : minute;
-        let secondes = date.getSeconds();
-        secondes = secondes < 10 ? '0' + secondes : secondes;
-        hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hour, 0, 12, 0, 360)}deg)`;
-        minuteEl.style.transform = `translate(-50%, -100%) rotate(${minute*6}deg)`; //也可以暴力计算角度
-        secondsEl.style.transform = `translate(-50%, -100%) rotate(${secondes*6}deg)`;
-        shijian.innerHTML = `${hour}:${minute}`;
-        dataEl.innerHTML = `${days[weeks]}, ${months[month]} <span class="circle">${day}</span>`
-        showTime(hour);
-    }
-    //点击事件
-btn.addEventListener('click', () => {
-    btn.classList.toggle('qiehuan');
-    body.classList.toggle('beijingqiehuan');
-    time.classList.toggle('beijingqiehuan');
-    hourEl.classList.toggle('beijingqiehuan');
-});
-//角度计算公式
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const loading = document.querySelector('.loading');
+const countdown = document.querySelector('.countdown');
+let nowyear = new Date().getFullYear();
+const newYearTime = new Date(`January 01 ${nowyear + 1} 00:00:00`);
+year.innerHTML = `${nowyear+1}`;
+
+
+function updateCountdown() {
+    const nowTime = new Date();
+    const relective = newYearTime - nowTime;
+    let day = Math.floor(relective / 1000 / 60 / 60 / 24);
+    day = day >= 10 ? day : `0${day}`; //bling
+    let hour = Math.floor(relective / 1000 / 60 / 60 % 24);
+    hour = hour >= 10 ? hour : `0${hour}`; //bling
+    let minute = Math.floor(relective / 1000 / 60 % 60);
+    minute = minute >= 10 ? minute : `0${minute}`; //bling
+    let second = Math.floor(relective / 1000 % 60);
+    second = second >= 10 ? second : `0${second}`; //bling
+    days.innerHTML = day;
+    hours.innerHTML = hour;
+    minutes.innerHTML = minute;
+    seconds.innerHTML = second;
 }
-setDate();
-setInterval(setDate, 1000);
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000); //嘿嘿
+setInterval(updateCountdown, 1000);
